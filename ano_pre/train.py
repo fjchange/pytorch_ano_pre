a98d16532db6d44878384ff4fe64ef2a5c675dd6
kiwi_fung <fjchange@hotmail.com>
2019-04-06 22:55:42 +0800

    Update train.py

--- ano_pre/train.py
+++ ano_pre/train.py
@@ -7,7 +7,7 @@ from losses import *
 
 sys.path.append('..')
 from Dataset import img_dataset
-from models.unet import UNet
+from models.unet import UNet,_test
 from models.pix2pix_networks import PixelDiscriminator
 from liteFlownet.lite_flownet import Network,batch_estimate
 from torch.utils.data import DataLoader
@@ -18,16 +18,21 @@ from tensorboardX import SummaryWriter
 from utils import utils
 import os
 
+from evaluate import evaluate
+#your gpu id
+torch.cuda.set_device(2)
+
+
 training_data_folder='your_path'
 testing_data_folder='your_path'
 
-writer_path='your_path'
-model_generator_save_path='your_path'
-model_discriminator_save_path='your_path'
+writer_path='../log/ano_pred_avenue_2'
 
-lite_flow_model_path='your_path'
+model_generator_save_path='../pth_model/ano_pred_avenue_generator_2.pth'
+model_discriminator_save_path='../pth_model/ano_pred_avenue_discriminator_2.pth'
 
-os.environ['CUDA_VISIBLE_DEVICES']='0,1'
+lite_flow_model_path='../liteFlownet/network-default.pytorch'
+
 
 #----------- all the param as ano pred said ---------
 batch_size=4
@@ -38,9 +43,13 @@ pretrain=False
 g_lr=0.0002
 d_lr=0.00002
 
-lam_int=1.0
-lam_gd=1.0
+#different range with the source version, should change
+lam_int=1.0*2
+lam_gd=1.0*2
+# the output of the flow keep same,
+# more precise the flow is, the more small op_rate?
 lam_op=2.0
+
 lam_adv=0.05
 
 #for gradient loss
@@ -63,7 +72,8 @@ def weights_init_normal(m):
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
 
-def train(frame_num,layer_nums,input_channels,output_channels,discriminator_num_filters,bn=False):
+def train(frame_num,layer_nums,input_channels,output_channels,discriminator_num_filters,bn=False,pretrain=False,
+          generator_pretrain_path=None,discriminator_pretrain_path=None):
     generator=UNet(n_channels=input_channels,layer_nums=layer_nums,output_channel=output_channels,bn=bn)
     discriminator=PixelDiscriminator(output_channels,discriminator_num_filters,use_norm=False)
 
@@ -79,10 +89,17 @@ def train(frame_num,layer_nums,input_channels,output_channels,discriminator_num_
     gd_loss=Gradient_Loss(alpha,num_channels).cuda()
     op_loss=Flow_Loss().cuda()
     int_loss=Intensity_Loss(l_num).cuda()
+    step = 0
 
     if not pretrain:
         generator.apply(weights_init_normal)
         discriminator.apply(weights_init_normal)
+    else:
+        assert (generator_pretrain_path!=None and discriminator_pretrain_path!=None)
+        generator.load_state_dict(torch.load(generator_pretrain_path))
+        discriminator.load_state_dict(torch.load(discriminator_pretrain_path))
+        step=int(generator_pretrain_path.split('-')[-1])
+        print('pretrained model loaded!')
 
     print('initializing the model with Generator-Unet {} layers,'
           'PixelDiscriminator with filters {} '.format(layer_nums,discriminator_num_filters))
@@ -99,7 +116,6 @@ def train(frame_num,layer_nums,input_channels,output_channels,discriminator_num_
     test_dataset=img_dataset.ano_pred_Dataset(testing_data_folder,frame_num)
     test_dataloader=DataLoader(dataset=test_dataset,batch_size=batch_size,shuffle=True,num_workers=1,drop_last=True)
 
-    step = 0
     for epoch in range(epochs):
         for (input,_),(test_input,_) in zip(dataset_loader,test_dataloader):
             # generator = generator.train()
@@ -129,6 +145,7 @@ def train(frame_num,layer_nums,input_channels,output_channels,discriminator_num_
             g_int_loss=int_loss(G_output,target)
             g_gd_loss=gd_loss(G_output,target)
 
+            lam_op=0
             g_loss=lam_adv*g_adv_loss+lam_gd*g_gd_loss+lam_op*g_op_loss+lam_int*g_int_loss
 
             optimizer_G.zero_grad()
@@ -154,29 +171,44 @@ def train(frame_num,layer_nums,input_channels,output_channels,discriminator_num_
 
             if step%10==0:
                 print("[{}/{}]: g_loss: {} d_loss {}".format(step,epoch,g_loss,d_loss))
-                print('\t gd_loss {}, op_loss {}, int_loss {}'.format(g_gd_loss,g_op_loss,g_int_loss))
+                print('\t gd_loss {}, op_loss {}, int_loss {} ,'.format(g_gd_loss,g_op_loss,g_int_loss))
                 print('\t train psnr{}，test_psnr {}'.format(train_psnr,test_psnr))
 
-                writer.add_scalar('train_psnr', train_psnr, global_step=step)
-                writer.add_scalar('test_psnr', test_psnr, global_step=step)
+                writer.add_scalar('psnr/train_psnr', train_psnr, global_step=step)
+                writer.add_scalar('psnr/test_psnr', test_psnr, global_step=step)
 
-                writer.add_scalar('g_loss', g_loss, global_step=step)
-                writer.add_scalar('d_loss', d_loss, global_step=step)
-                writer.add_scalar('adv_loss', g_adv_loss, global_step=step)
-                writer.add_scalar('op_loss', g_op_loss, global_step=step)
-                writer.add_scalar('int_loss', g_int_loss, global_step=step)
-                writer.add_scalar('gd_loss', g_gd_loss, global_step=step)
+                writer.add_scalar('total_loss/g_loss', g_loss, global_step=step)
+                writer.add_scalar('total_loss/d_loss', d_loss, global_step=step)
+                writer.add_scalar('g_loss/adv_loss', g_adv_loss, global_step=step)
+                writer.add_scalar('g_loss/op_loss', g_op_loss, global_step=step)
+                writer.add_scalar('g_loss/int_loss', g_int_loss, global_step=step)
+                writer.add_scalar('g_loss/gd_loss', g_gd_loss, global_step=step)
 
-                writer.add_image('train_target', target[0], global_step=step)
-                writer.add_image('train_output', G_output[0], global_step=step)
-                writer.add_image('test_target', test_target[0], global_step=step)
-                writer.add_image('test_output', test_output[0], global_step=step)
+                writer.add_image('image/train_target', target[0], global_step=step)
+                writer.add_image('image/train_output', G_output[0], global_step=step)
+                writer.add_image('image/test_target', test_target[0], global_step=step)
+                writer.add_image('image/test_output', test_output[0], global_step=step)
 
-            if step%1000==0:
-                utils.saver(generator.state_dict(),model_generator_save_path,step)
-                utils.saver(discriminator.state_dict(),model_discriminator_save_path,step)
             step+=1
+
+            if step%500==0:
+                utils.saver(generator.state_dict(),model_generator_save_path,step,max_to_save=10)
+                utils.saver(discriminator.state_dict(),model_discriminator_save_path,step,max_to_save=10)
+                if step>=2000:
+                    print('==== begin evaluate the model of {} ===='.format(model_generator_save_path+'-'+str(step)))
+
+                    auc=evaluate(frame_num=5,layer_nums=4,input_channels=12,output_channels=3,
+                             model_path=model_generator_save_path+'-'+str(step),evaluate_name='compute_auc')
+                    writer.add_scalar('results/auc', auc, global_step=step)
+
 
 if __name__=='__main__':
     train(num_clips,num_unet_layers,num_channels*(num_clips-num_his),num_channels,discriminator_channels)
+        # pretrain=True,
+        # generator_pretrain_path='../pth_model/ano_pred_avenue_generator_2.pth-4500',
+        # discriminator_pretrain_path='../pth_model/ano_pred_avenue_discriminator_2.pth-4500')
+
+    #test(num_clips,num_unet_layers,num_channels*(num_clips-num_his),num_channels,discriminator_channels)
+    #_test()
+    #test(0,0,0,0,0)
 
